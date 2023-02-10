c3d84a643ec558009529890b7cba650cba3baeac
MartinAleks <martinaleks@gmail.com>
2023-02-10 12:07:57 +0200

    Routing

---
+++ veebipood/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import Avaleht from './pages/Avaleht';
+import Ostukorv from './pages/Ostukorv';
+import LisaToode from './pages/LisaToode';
+
+// HTML elemente on 2 erinevat:
+// 1. algus ja lõpu tag, lõpp on kaldkriipsuga tag nimetuse ees
+//  kõige standardsem, omadused/seadistused alguse sisse
+// 2. self-closing, tagi nimetuse järel on kaldkriips
+//  kui me teame ette, et midagi kunagi kahe tagi vahele
+// pole mõtet kirjutada
+
+function App() {
+  return (
+    <div className="App">
+      <Link to="/">
+      <img src="https://i.pinimg.com/550x/d2/50/c9/d250c95a44debaec2c4239ff486164bc.jpg" alt="Italian Trulli"/>
+      </Link>
+
+      
+      <Link to="/ostukorv">
+      <button className="nupp">Nupp</button>
+      </Link>
+
+      <Link to="/lisa-toode">
+      <button className="nupp">Nupp</button>
+      </Link>
+
+      {/* localhost:3000/avaleht */}
+      <Routes>
+        <Route path="" element={ <Avaleht/>}/>
+        <Route path="ostukorv" element={ <Ostukorv/> }/>
+        <Route path="lisa-toode" element={ <LisaToode/> }/>
+      </Routes>
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ veebipood/src/pages/Avaleht.js
@@ -0,0 +1,31 @@
+//rfce tekitab template!!
+
+import React, { useState } from 'react'
+
+function Avaleht() {
+    const [kogus, uuendaKogus] = useState(12);
+
+    function nulli() {
+        uuendaKogus(0);
+    }
+
+    function vahenda() {
+        uuendaKogus(kogus -1);
+    }
+
+    function suurenda() {
+        uuendaKogus(kogus +1);
+    }
+
+  return (
+    <div>
+        <button onClick={nulli}>Nulli</button>
+        <button onClick={vahenda}>-</button>
+        {kogus}
+        <button onClick={suurenda}>+</button>
+    </div>
+  )
+}
+
+export default Avaleht
+
