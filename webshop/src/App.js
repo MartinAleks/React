f9818ecdd386861c26c35ac683e7b164f6f99124
MartinAleks <martinaleks@gmail.com>
2023-03-13 16:25:31 +0200

    Tõlge, ebay tooted, bootstrap

--- webshop/src/App.js
+++ webshop/src/App.js
@@ -1,15 +1,20 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
-import HomePage from "./pages/HomePage";
-import Cart from "./pages/Cart";
-import Shop from "./pages/Shop";
-import ContactUs from "./pages/ContactUs";
-import AdminHome from "./pages/AdminHome";
-import AddProduct from "./pages/AddProduct";
-import EditProduct from "./pages/EditProduct";
-import MaintainProducts from "./pages/MaintainProducts";
-import MaintainShops from "./pages/MaintainShops";
-import MaintainCategories from "./pages/MaintainCategories";
+import HomePage from "./pages/public/HomePage";
+import Cart from "./pages/public/Cart";
+import Shop from "./pages/public/Shop";
+import ContactUs from "./pages/public/ContactUs";
+import AdminHome from "./pages/admin/AdminHome";
+import AddProduct from "./pages/admin/AddProduct";
+import EditProduct from "./pages/admin/EditProduct";
+import MaintainProducts from "./pages/admin/MaintainProducts";
+import MaintainShops from "./pages/admin/MaintainShops";
+import MaintainCategories from "./pages/admin/MaintainCategories";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { useTranslation } from 'react-i18next';
+
 
 
 
@@ -26,12 +31,36 @@ import MaintainCategories from "./pages/MaintainCategories";
 // Toastify
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const updateLang = (newLang) => {
+    i18n.changeLanguage(newLang);
+    localStorage.setItem("language", newLang);
+  }
+
   return (
     <div className="App">
+      <h1>{t('Welcome to React')}</h1>
+      
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">Webshop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/admin">{t("admin")}</Nav.Link>
+            <Nav.Link as={Link} to="/cart">{t("cart")}</Nav.Link>
+            <Nav.Link as={Link} to="/contact">{t("contact")}</Nav.Link>
+            <Nav.Link as={Link} to="/shops">{t("shops")}</Nav.Link>
+          </Nav>
+          <img className="lang" src="/estonia.png" alt="" onClick={() => updateLang("ee")}/>
+          <img className="lang" src="/english.png" alt="" onClick={() => updateLang("en")}/>
+          
+        </Container>
+      </Navbar>
+
       <Routes>
         <Route path="" element={ <HomePage />} />
         <Route path="cart" element={ <Cart />} />
-        <Route path="shop" element={ <Shop />} />
+        <Route path="shops" element={ <Shop />} />
         <Route path="contact" element={ <ContactUs />} />
         <Route path="admin" element={ <AdminHome />} />
         <Route path="admin/add-product" element={ <AddProduct />} />
--- webshop/src/index.js
+++ webshop/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import './i18n';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
-
-// npm install react-router-dom
-// BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
